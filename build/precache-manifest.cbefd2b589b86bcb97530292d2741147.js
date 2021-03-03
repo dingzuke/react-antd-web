79ac022113d2fb1c60ce3a40680665e2a0c84e34
丁祖科 <dingzuke@ainnovation.com>
2021-03-03 16:27:48 +0800

    feat: 上传打包文件

---
+++ build/precache-manifest.cbefd2b589b86bcb97530292d2741147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28ea878cc3c69016bf8d",
+    "url": "/static/css/main.21a2cc7f.chunk.css"
+  },
+  {
+    "revision": "28ea878cc3c69016bf8d",
+    "url": "/static/js/main.28ea878c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "3e8d95f80940432c7ee1",
+    "url": "/static/css/2.2215bd16.chunk.css"
+  },
+  {
+    "revision": "3e8d95f80940432c7ee1",
+    "url": "/static/js/2.3e8d95f8.chunk.js"
+  },
+  {
+    "revision": "a47e09eeae7dc5ccb9bce8d3ab7ef878",
+    "url": "/index.html"
+  }
+];
